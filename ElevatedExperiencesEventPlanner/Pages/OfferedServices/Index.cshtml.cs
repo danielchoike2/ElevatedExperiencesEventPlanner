0f4dd0e7d05d66a7e4516636c55f6bd56c116c62
danielchoike2 <choike3@mail.nmc.edu>
2023-11-29 19:31:50 -0500

    Add project files.

---
+++ ElevatedExperiencesEventPlanner/Pages/OfferedServices/Index.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ElevatedExperiencesEventPlanner.Data;
+using ElevatedExperiencesEventPlanner.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+
+namespace ElevatedExperiencesEventPlanner.Pages.OfferedServices
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ElevatedExperiencesEventPlanner.Data.ElevatedExperiencesEventPlannerContext _context;
+
+        public IndexModel(ElevatedExperiencesEventPlanner.Data.ElevatedExperiencesEventPlannerContext context)
+        {
+            _context = context;
+        }
+
+        public IList<OfferedService> OfferedService { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        public SelectList? ServiceNames { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? ServiceName { get; set; }
+        
+        public string NameSort { get; set; }
+        public async Task OnGetAsync(string sortOrder)
+        {
+            NameSort = sortOrder;
+            IQueryable<OfferedService> offeredServiceQuery = from s in _context.OfferedService
+                                                             select s;
+
+            var offeredServices = from s in _context.OfferedService
+                         select s;
+            
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                offeredServices = offeredServices.Where(s => s.Name.Contains(SearchString));
+            }
+            if (NameSort == "desc")
+            {
+                offeredServices = offeredServices.OrderByDescending(e => e.Name);
+
+            }
+            else
+            {
+                offeredServices = offeredServices.OrderBy(offeredServices => offeredServices.Name);
+            }
+
+            OfferedService = await offeredServices.ToListAsync();
+        }
+    }
+}
---
+++ ElevatedExperiencesEventPlanner/Pages/Schedules/Index.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ElevatedExperiencesEventPlanner.Data;
+using ElevatedExperiencesEventPlanner.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ElevatedExperiencesEventPlanner.Pages.Schedules
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ElevatedExperiencesEventPlanner.Data.ElevatedExperiencesEventPlannerContext _context;
+
+        public IndexModel(ElevatedExperiencesEventPlanner.Data.ElevatedExperiencesEventPlannerContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Schedule> Schedule { get;set; } = default!;
+
+        
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        public SelectList? ServiceNames { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? ServicesName { get; set; }
+
+        public string NameSort { get; set; }
+
+
+        public async Task OnGetAsync(string sortOrder)
+        {
+            NameSort = sortOrder;
+            IQueryable<Schedule> scheduleQuery = from s in _context.Schedule
+                                                             select s;
+
+
+            var schedules = from s in _context.Schedule
+                         select s;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                schedules = schedules.Where(s => s.ServiceName.Contains(SearchString));
+            }
+
+            if (NameSort == "desc")
+            {
+                schedules = schedules.OrderByDescending(e => e.ScheduleDate);
+
+            }
+            else
+            {
+                schedules = schedules.OrderBy(offeredServices => offeredServices.ScheduleDate);
+            }
+
+            Schedule = await schedules.ToListAsync();
+        }
+    }
+}
